b151adde5bacaabd15b04087bd8a8d16e98ec47f
NahuelALB <ivanalbarado8@gmail.com>
2023-07-05 20:41:01 -0300

    Corregí el console.log linea 6

--- numero3.js
+++ numero3.js
@@ -3,8 +3,8 @@ function resto(a, b) {
 
     return holaSoyEl;
 };
-console.log(resto((15,5)));
-console.log("El resto entre 15 y 5 es: "+resto(15,5));
+console.log(resto(15, 5));
+console.log("El resto entre 15 y 5 es: " + resto(15, 5));
 
 console.log(resto(1000, 23213));
-console.log("El resto entre 1000 y 23213 es: "+resto(1000,23213));
+console.log("El resto entre 1000 y 23213 es: " + resto(1000, 23213));
